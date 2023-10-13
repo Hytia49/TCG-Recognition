2a3d50b2034f442eb8d705ecdcf094a29ddff281
Lambert Clara <claralambert@empovet.com>
2023-10-13 17:01:47 +0200

    connexion code to db azure

---
+++ toolbox/azure_db_connexion.py
@@ -0,0 +1,17 @@
+import pypyodbc as odbc
+import textwrap
+import pandas as pd
+import sys
+import os
+
+# Local imports
+parent = os.path.dirname(os.path.dirname(os.path.realpath(__file__))) 
+sys.path.append(parent)
+
+from settings import DRIVER, SERVER, DATABASE, USERNAME, PASSWORD
+
+
+connection_string = f"Driver={DRIVER};Server={SERVER},1433;Database={DATABASE};Uid={USERNAME};Pwd={PASSWORD};Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;"
+
+print(connection_string)
+conn = odbc.connect(connection_string)
