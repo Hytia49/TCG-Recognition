55a3f78cd6aacac673747e02d535cbcfa9e4a6c9
Lambert Clara <claralambert@empovet.com>
2023-10-13 17:00:19 +0200

    add conf.ini settings with parser

--- settings.py
+++ settings.py
@@ -1,3 +1,4 @@
+from configparser import ConfigParser
 import os
 from pathlib import Path
 
@@ -5,3 +6,17 @@ DIR_PATH = Path(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 
 DF_PATH = os.path.join(DIR_PATH,'tcg-recognition','data', 'processed')
+
+
+# Azure db part
+
+CONF_INI_PATH = os.path.join(DIR_PATH, 'tcg-recognition')
+
+parser = ConfigParser()
+parser.read(f"{CONF_INI_PATH}\conf.ini")
+
+DRIVER = parser['settings']['driver']
+SERVER = parser['settings']['server']
+DATABASE = parser['settings']['database']
+USERNAME = parser['settings']['username']
+PASSWORD = parser['settings']['password']
