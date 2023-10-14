b5c17414d222849fc514744e216a857bc73d6302
Lambert Clara <claralambert@empovet.com>
2023-10-14 19:19:16 +0200

    request on azure database

--- requests/SQLQuery1.sql
+++ requests/SQLQuery1.sql
@@ -1,7 +1,7 @@
-CREATE TABLE Pokemoncardfr (
-	Id INT,
-	Name VARCHAR(50),
-	Number VARCHAR(50),
-	Serie VARCHAR(50),
-	URL_img VARCHAR(100)
+CREATE TABLE Pokemoncardsfr (
+    Id INT PRIMARY KEY,
+    Name VARCHAR(50),
+    Number VARCHAR(50),
+    Serie VARCHAR(50),
+    Url_img VARCHAR(100)
 );
